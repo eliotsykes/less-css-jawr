741878b26de8c982901034dc9295fe35852efc99
Tim Kingman <tkingman@hcreit.com>
2013-05-03 14:51:15 -0400

    Remove duplicated code, only keep URL method, update tests.

--- src/main/java/com/jetbootlabs/jawr/LessCssResourceGenerator.java
+++ src/main/java/com/jetbootlabs/jawr/LessCssResourceGenerator.java
@@ -7,7 +7,6 @@ import com.asual.lesscss.LessException;
 
 import java.io.Reader;
 import java.io.StringReader;
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -36,19 +35,6 @@ public class LessCssResourceGenerator implements ResourceGenerator {
         }
         String css = generateCssFromLessFile(resource);
         return new StringReader(css);
-    }
-
-    public String generateCssFromLessFile(String path) {
-        if (!path.toLowerCase().endsWith(".less") && !path.toLowerCase().endsWith(".css")) {
-            throw new SecurityException("Attempt to read non-style file '" + path + "' was made in an odd place, mmm...");
-        }
-        File lessFile = new File(path);
-        try {
-            String css = engine.compile(lessFile);
-            return css;
-        } catch (LessException e) {
-            throw new RuntimeException("Problem compiling Less CSS from file '" + path + "'", e);
-        }
     }
 
     public String generateCssFromLessFile(URL url) {
--- src/test/java/com/jetbootlabs/jawr/LessCssResourceGeneratorTest.java
+++ src/test/java/com/jetbootlabs/jawr/LessCssResourceGeneratorTest.java
@@ -4,29 +4,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LessCssResourceGeneratorTest {
 
     private LessCssResourceGenerator generator = new LessCssResourceGenerator();
 
     @Test
-    public void testLessCssProcessedIntoCss() {
+    public void testLessCssUrlProcessedIntoCss() throws MalformedURLException {
         String lineSep = System.getProperty("line.separator");
         String expectedCss =
                 ".style1 {" + lineSep +
                         "  height: 10px;" + lineSep +
                         "}" + lineSep;
         String pathToLessFile = "src/test/resources/test.less";
-        String css = generator.generateCssFromLessFile(pathToLessFile);
+        URL urlToLessFile = new File(pathToLessFile).toURI().toURL();
+        String css = generator.generateCssFromLessFile(urlToLessFile);
         assertEquals(expectedCss.replaceAll("\r", ""), css.replaceAll("\r", ""));
     }
 
     @Test
-    public void testNonStyleFileExtensionsThrowSecurityException() {
+    public void testNonStyleFileExtensionsThrowSecurityException() throws MalformedURLException {
         String[] nonStylePaths = {"WEB-INF/web.xml", "/some/path/secret.less.txt", "/tmp/moresecrets.css.yml"};
         for (int i = 0; i < nonStylePaths.length; i++) {
             String path = nonStylePaths[i];
             try {
-                generator.generateCssFromLessFile(path);
+                URL url = new File(path).toURI().toURL();
+                generator.generateCssFromLessFile(url);
                 fail("Exception should have been thrown for non-style path '" + path + "'");
             } catch (SecurityException e) {
             }
